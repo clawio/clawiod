2141d1d234af92c0e1883dcf3bea817f6ec45511
Hugo González Labrador <github@hugo.labkode.com>
2015-09-20 12:08:46 +0200

    Provided signaler interface with signalone as implementation

--- pkg/signaler/signaler.go
+++ pkg/signaler/signaler.go
@@ -10,63 +10,6 @@
 // Package signaler listen to OS signals to manage the life cycle of the daemon and reloading of configuration files.
 package signaler
 
-import (
-	"fmt"
-	"github.com/clawio/clawiod/pkg/apiserver"
-	"github.com/clawio/clawiod/pkg/config"
-	"os"
-	"os/signal"
-	"syscall"
-)
-
 type Signaler interface {
 	Start() <-chan bool
-}
-
-type signaler struct {
-	srv  apiserver.APIServer
-	sigc chan os.Signal
-	endc chan bool
-	cfg  config.Config
-}
-
-func New(srv apiserver.APIServer, cfg config.Config) Signaler {
-	sigc := make(chan os.Signal, 1)
-	endc := make(chan bool, 1)
-	return &signaler{cfg: cfg, srv: srv, sigc: sigc, endc: endc}
-}
-func (s *signaler) Start() <-chan bool {
-	go func() {
-		signal.Notify(s.sigc,
-			syscall.SIGINT,
-			syscall.SIGTERM,
-			syscall.SIGHUP,
-			syscall.SIGQUIT,
-		)
-
-		for {
-			sig := <-s.sigc
-			switch sig {
-			case syscall.SIGHUP:
-				err := s.cfg.Reload()
-				if err != nil {
-					fmt.Println("SIGHUP received: Error reloading the configuration ", err)
-				}
-				fmt.Println("SIGHUP received: Configuration reloaded")
-			case syscall.SIGINT:
-				fmt.Println("SIGINT received: Hard shutdown")
-				s.endc <- true
-			case syscall.SIGTERM:
-				fmt.Println("SIGTERM received: Hard shutdown")
-				s.endc <- true
-			case syscall.SIGQUIT:
-				stop := s.srv.StopChan()
-				s.srv.Stop()
-				<-stop
-				fmt.Println("SIGQUIT: Graceful shutdown")
-				s.endc <- true
-			}
-		}
-	}()
-	return s.endc
 }
