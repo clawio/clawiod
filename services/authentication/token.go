6f5afd0e00d381122e5dcbc173cad569726a526c
Hugo Gonzalez <github@hugo.labkode.com>
2016-05-26 18:04:21 +0200

    Add logging to authentication.token

--- services/authentication/token.go
+++ services/authentication/token.go
@@ -2,6 +2,7 @@ package authentication
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/clawio/clawiod/codes"
@@ -24,36 +25,45 @@ type (
 // Authenticate authenticates an user using an username and a password.
 func (s *svc) Token(w http.ResponseWriter, r *http.Request) {
 	log := keys.MustGetLog(r)
+
 	if r.Body == nil {
-		log.Error("body is nil")
+		log.WithError(errors.New("body is nil")).Info("cannot read body")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	authReq := &TokenRequest{}
 	if err := json.NewDecoder(r.Body).Decode(authReq); err != nil {
 		e := codes.NewErr(codes.BadInputData, "")
+		log.WithError(e).Error(codes.BadInputData.String())
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(e)
+		if err := json.NewEncoder(w).Encode(e); err != nil {
+			log.WithError(err).Error("cannot encode")
+		}
 		return
 	}
+
 	token, err := s.authenticationController.Authenticate(authReq.Username, authReq.Password)
 	if err != nil {
 		s.handleTokenError(err, w, r)
 		return
 	}
+	log.WithField("user", authReq.Username).Info("token generated")
+
 	res := &TokenResponse{AccessToken: token}
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(res); err != nil {
-		log.Error(err)
+		log.WithError(err).Error("cannot encode")
 	}
 }
 
 func (s *svc) handleTokenError(err error, w http.ResponseWriter, r *http.Request) {
 	log := keys.MustGetLog(r)
 	e := codes.NewErr(codes.BadInputData, "user or password do not match")
+	log.WithError(e).Error(codes.BadInputData.String())
 	w.WriteHeader(http.StatusBadRequest)
 	if err := json.NewEncoder(w).Encode(e); err != nil {
-		log.Error(err)
+		log.WithError(err).Error("cannot encode")
 	}
 	return
 }
