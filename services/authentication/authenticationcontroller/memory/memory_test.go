45e6ed5fbabe00b63b6d0c0703afde6c9d739105
Hugo Gonzalez <github@hugo.labkode.com>
2016-06-20 09:36:24 +0200

    Adapt memory tests

--- services/authentication/authenticationcontroller/memory/memory_test.go
+++ services/authentication/authenticationcontroller/memory/memory_test.go
@@ -3,52 +3,90 @@ package memory
 import (
 	"testing"
 
+	"github.com/clawio/clawiod/config"
+	"github.com/clawio/clawiod/config/default"
+	mock_configsource "github.com/clawio/clawiod/config/mock"
 	"github.com/clawio/clawiod/entities"
 	"github.com/clawio/clawiod/services/authentication/authenticationcontroller"
-	"github.com/clawio/clawiod/services/authentication/lib"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
-var users = []User{
-	{User: entities.User{Username: "test"}, Password: "test"},
-	{User: entities.User{Username: "hugo"}, Password: "hugo"},
+var defaultDirs = defaul.DefaultDirectives
+
+type testObject struct {
+	authenticationController       authenticationcontroller.AuthenticationController
+	memoryAuthenticationController *controller
+	mockSource                     *mock_configsource.Source
+	conf                           *config.Config
+	user                           *entities.User
 }
 
-type TestSuite struct {
-	suite.Suite
-	authenticationController authenticationcontroller.AuthenticationController
-	controller               *controller
+func newObject(t *testing.T) *testObject {
+	mockSource := &mock_configsource.Source{}
+	conf := config.New([]config.Source{mockSource})
+
+	o := &testObject{}
+	o.mockSource = mockSource
+	o.conf = conf
+	o.user = &entities.User{Username: "test"}
+
+	return o
 }
 
-func Test(t *testing.T) {
-	suite.Run(t, new(TestSuite))
-}
-func (suite *TestSuite) SetupTest() {
-	opts := &Options{
-		Users:         users,
-		Authenticator: lib.NewAuthenticator("secret", "HS256"),
-	}
-	authenticationController := New(opts)
-	require.NotNil(suite.T(), authenticationController)
-	suite.authenticationController = authenticationController
-	suite.controller = suite.authenticationController.(*controller)
+func (o *testObject) loadDirs(t *testing.T, dirs *config.Directives) {
+	o.mockSource.On("LoadDirectives").Return(dirs, nil)
+	err := o.conf.LoadDirectives()
+	require.Nil(t, err)
+
 }
 
-func (suite *TestSuite) TestNew() {
-	opts := &Options{
-		Users:         users,
-		Authenticator: lib.NewAuthenticator("secret", "HS256"),
-	}
-	c := New(opts)
-	require.NotNil(suite.T(), c)
+func (o *testObject) setupController(t *testing.T, dirs *config.Directives) {
+	o.loadDirs(t, dirs)
+	c, err := New(o.conf)
+	require.Nil(t, err)
+	o.authenticationController = c
+	o.memoryAuthenticationController = o.authenticationController.(*controller)
 }
 
-func (suite *TestSuite) TestAuthenticate() {
-	_, err := suite.authenticationController.Authenticate("test", "test")
-	require.Nil(suite.T(), err)
+func TestNew(t *testing.T) {
+	dirs := defaultDirs
+	o := newObject(t)
+	o.loadDirs(t, &dirs)
+
+	_, err := New(o.conf)
+	require.Nil(t, err)
 }
-func (suite *TestSuite) TestAuthenticate_withBadUser() {
-	_, err := suite.authenticationController.Authenticate("notfound", "notfound")
-	require.NotNil(suite.T(), err)
+
+func TestNew_withBadJSON(t *testing.T) {
+	dirs := defaultDirs
+	dirs.Authentication.Memory.Users = "this is a string"
+	o := newObject(t)
+	o.loadDirs(t, &dirs)
+
+	_, err := New(o.conf)
+	require.NotNil(t, err)
+}
+
+func TestAuthenticate(t *testing.T) {
+	dirs := defaultDirs
+	dirs.Authentication.Memory.Users = []map[string]interface{}{map[string]interface{}{"username": "test", "password": "test"}}
+	o := newObject(t)
+	o.setupController(t, &dirs)
+
+	_, err := o.authenticationController.Authenticate("test", "test")
+	require.Nil(t, err)
+}
+func TestAuthenticate_withBadUser(t *testing.T) {
+	dirs := defaultDirs
+	o := newObject(t)
+	o.setupController(t, &dirs)
+
+	_, err := o.authenticationController.Authenticate("notfound", "notfound")
+	require.NotNil(t, err)
+}
+
+func TestDecode_withBadValue(t *testing.T) {
+	c := make(chan string)
+	_, err := decodeUsers(c)
+	require.NotNil(t, err)
 }
